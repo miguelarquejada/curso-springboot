aad9b2b29ae2e35f9acfbe2f4af7573abefdcc00
Miguel Arquejada <miguelarbarquejada@gmail.com>
2021-01-22 17:20:31 -0300

    Service layer, component registration

---
+++ src/main/java/com/marb/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.marb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marb.course.entities.User;
+import com.marb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
