7746c2a82e01a6dce6ee24d1f88361d4a0b27779
Miguel Arquejada <miguelarbarquejada@gmail.com>
2021-01-24 00:25:14 -0300

    Product

--- src/main/java/com/marb/course/config/TestConfig.java
+++ src/main/java/com/marb/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.marb.course.entities.Category;
 import com.marb.course.entities.Order;
+import com.marb.course.entities.Product;
 import com.marb.course.entities.User;
 import com.marb.course.entities.enums.OrderStatus;
 import com.marb.course.repositories.CategoryRepository;
 import com.marb.course.repositories.OrderRepository;
+import com.marb.course.repositories.ProductRepository;
 import com.marb.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -45,5 +50,11 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1); 
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 }
