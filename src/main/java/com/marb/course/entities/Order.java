2686a675ee677990e624d266f4d6e9a299025dfd
Miguel Arquejada <miguelarbarquejada@gmail.com>
2021-01-26 17:26:58 -0300

    Subtotal & Total methods

--- src/main/java/com/marb/course/entities/Order.java
+++ src/main/java/com/marb/course/entities/Order.java
@@ -96,6 +96,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double getTotal() {
+		Double total = 0.0;
+		for(OrderItem item : items) {
+			total += item.getSubTotal();
+		}
+		return total;
+	}
 
 	@Override
 	public int hashCode() {
